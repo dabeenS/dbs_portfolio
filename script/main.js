fc7b14705c62e6bd242b3c0850dc608b646fb8ab
dabeenS <smscms469050@gmail.com>
2023-11-20 14:20:52 +0900

    index_v1

---
+++ script/main.js
@@ -0,0 +1,52 @@
+$(function(){
+  
+
+  // 인트로 애니메이션
+
+  function introAni() {
+    $('.content_wrap').css({display: 'block'});
+    $('.intro').css({background: 'transparent', position: 'fixed' , border: '24px solid var(--point)'});
+  }
+  
+  setTimeout(() => {
+    $('.intro .txt_area').css({opacity: 0});
+  }, 400 );
+
+  setTimeout(() => {
+    introAni()
+  }, 600 );
+
+  setTimeout(() => {
+    $('.intro').css({display: 'none'})
+  }, 1000 );
+
+
+  // 작업물 변경
+
+  $('.work._1 button').click(function(){
+    $(this).parents('.work').removeClass('on');
+    $('.work._2').addClass('on');
+  });
+
+  $('.work._2 button').click(function(){
+    $(this).parents('.work').removeClass('on');
+    $('.work._1').addClass('on');
+  });
+
+
+  // 좌측 하단 메뉴 숨기기
+
+  $(window).scroll(function () {
+
+    var foot = $('#contact').position().top - 760;
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= foot) {
+      $('nav').addClass('off');
+    } else {
+      $('nav').removeClass('off');
+    }
+
+  });
+
+});
